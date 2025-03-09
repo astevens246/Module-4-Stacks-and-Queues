2f5510f9acc8855194724ecd444b4fd2cfd5286d
Allen Stevens <allen.stevens@students.dominican.edu>
2025-03-08 17:18:05 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from LinkedList import LinkedList
+from LinkedListStack import LinkedListStack
+from LinkedListQueue import LinkedListQueue
+
+'''ll = LinkedList()
+ll.append("A")
+ll.append("B")
+ll.append("C")
+ll.prepend("X")
+ll.prepend("Z")
+
+ll.delete_from_head()
+ll.delete_from_head()
+
+ll.delete_from_tail()
+
+ll.print_list()'''
+
+#TODO: test out stack
+
+#TODO: test out queue
